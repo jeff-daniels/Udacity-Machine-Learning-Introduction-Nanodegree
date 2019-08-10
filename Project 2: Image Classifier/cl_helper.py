bb3bd074ddbe462625afc5fb92f105c227fbed04
Jeff Daniels <36964220+jeff-daniels@users.noreply.github.com>
2019-08-09 20:57:30 -0400

    copy most code from .ipynb and helper.py

---
+++ Project 2: Image Classifier/cl_helper.py
@@ -0,0 +1,345 @@
+"""
+Helper functions for Image Classifier Project notebook
+
+"""
+
+import torch
+import time
+from torch import nn
+import torch.nn.functional as F
+from torchvision import models
+from PIL import Image
+import matplotlib.pyplot as plt
+
+class Network(nn.Module):
+    def __init__(self, input_size, output_size, hidden_layers, dropout_p):
+        '''
+        Builds a feedforward network with hidden layers
+        
+        Arguments
+        -----------
+        input_size: integer, size of the input layer
+        output_size: integer, size of the output layer
+        hidden_layers: list of integers, sizes of the hidden layers
+        '''
+        super().__init__()
+        # Input to a hidden layer
+        self.hidden_layers = nn.ModuleList([nn.Linear(input_size, hidden_layers[0])])
+        
+        # Add a varying number of additional hidden layers
+        layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
+        self.hidden_layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
+        
+        self.output = nn.Linear(hidden_layers[-1], output_size)
+
+        self.dropout = nn.Dropout(p= dropout_p)
+        
+    def forward(self, x):
+        ''' Forward pass through the networdk, returns the output logits '''
+        
+        for each in self.hidden_layers:
+            x = F.relu(each(x))
+            x = self.dropout(x)
+        x = self.output(x)
+        
+        return F.log_softmax(x, dim = 1)
+    
+def build_model(pre_trained_network=models.vgg16(pretrained=True),
+               input_size=25088, output_size=102, hidden_layers=[4096, 512], 
+               dropout_p = 0.5):
+
+    # Use GPU if it's available
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        torch.cuda.manual_seed_all(42)
+    else:
+        device = torch.device("cpu")
+        
+    # Load a pre-trained network
+    model = pre_trained_network
+    
+    # Freeze features parameters so backpropagation isn't performed
+    for param in model.parameters():
+        param.require_grad = False
+        
+    # Define a new, untrained feed-forward network as a classifier, using ReLU activations and dropout
+    model.classifier = Network(input_size, output_size, hidden_layers, dropout_p)
+
+    model.to(device)
+    
+    return model, device
+
+def validation(model, device, dataloader, criterion):
+    loss = 0
+    accuracy = 0
+    model.eval()
+
+    with torch.no_grad():
+        for inputs, labels in dataloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            logps = model.forward(inputs)
+            batch_loss = criterion(logps, labels)
+
+            loss += batch_loss.item()
+            
+            # Calculate accuracy
+            ps = torch.exp(logps)
+            top_p, top_class = ps.topk(1, dim=1)
+            equals = top_class == labels.view(*top_class.shape)
+            accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+    loss = loss/len(dataloader)
+    accuracy = accuracy/len(dataloader)
+    return loss, accuracy
+    
+def train(model, device, trainloader, validloader, criterion, optimizer, 
+          epochs=1, print_every=5, max_steps=None, performance_dict = {}):
+    # Train the classifier layers using backpropagation using the pre-trained network to get the features
+    steps = 0
+    running_loss = 0
+    running_accuracy = 0
+    train_losses, train_accuracies, valid_losses, valid_accuracies, epoch_durations = [], [], [], [], []
+    
+    for epoch in range(epochs):
+        start = time.time()
+        model.train()
+        if steps == max_steps:
+            break
+        for inputs, labels in trainloader:
+            steps += 1
+            # Stop early for testing
+            if steps == max_steps:
+                break
+                
+            # Move input and label tensors to the default device
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            # Calculate accuracy
+            ps = torch.exp(logps)
+            top_p, top_class = ps.topk(1, dim=1)
+            equals = top_class == labels.view(*top_class.shape)
+            running_accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+            # Print out in-training performance
+            if steps % print_every == 0:
+                model.eval()
+                
+                # Calculate performance
+                valid_loss, valid_accuracy = validation(model, device, validloader, criterion)
+                train_loss = running_loss/print_every
+                train_accuracy = running_accuracy/print_every
+                
+                # Update performance lists
+                train_losses.append(train_loss)
+                train_accuracies.append(train_accuracy)
+                valid_losses.append(valid_loss)
+                valid_accuracies.append(valid_accuracy)
+                
+                print(f"Epoch {epoch+1}/{epochs}.."
+                      f"Train loss: {train_loss/print_every:.3f}.."
+                      f"Validation loss: {valid_loss:.3f}.."
+                      f"Validation Accuracy: {valid_accuracy:.3f}"
+                     )
+                
+                running_loss = 0
+                running_accuracy =0
+                model.train()
+                
+        end = time.time()
+        epoch_durations.append(end-start)
+        print(f"Epoch duration: {end-start}")
+    
+    # Create or update performance_dict
+    keys = ['train_losses', 'train_accuracies', 'valid_losses', 'valid_accuracies', 'epoch_durations']
+    lsts = [train_losses, train_accuracies, valid_losses, valid_accuracies, epoch_durations]
+    for key, lst in zip(keys, lsts):
+        if key in performance_dict.keys():
+            for item in lst:
+                performance_dict[key].append(item)
+        else:
+            performance_dict[key] = lst
+    
+    num_epochs = len(performance_dict['epoch_durations'])
+    performance_dict['num_epochs'] = num_epochs
+    
+    # Create training_dict
+    input_size = model.classifier.hidden_layers[0].in_features
+    output_size = model.classifier.output.out_features
+    hidden_layers = []
+    for layer in model.classifier.hidden_layers:
+        hidden_layers.append(layer.out_features)
+
+    dropout_p = model.classifier.dropout.p
+    
+    for param_group in optimizer.param_groups:
+        lr = param_group['lr']
+    
+    training_dict = {'input_size':input_size, 'output_size':output_size, 
+                     'hidden_layers':hidden_layers,
+                     'criterion':criterion, 'optimizer':optimizer, 
+                     'lr':lr, 'dropout_p':dropout_p}
+
+    return performance_dict, training_dict
+
+def save_checkpoint(filepath, model, performance_dict, training_dict, mapping_dict):
+    
+    model.to('cpu')
+    
+    checkpoint = {'state_dict':model.state_dict(),
+                  'performance_dict':performance_dict, 
+                  'training_dict':training_dict, 
+                  'mapping_dict':mapping_dict}
+    torch.save(checkpoint, filepath)
+    
+    return None
+    
+def load_checkpoint(filepath, pre_trained_network=models.vgg16(pretrained=True)):
+    checkpoint = torch.load(filepath)
+    input_size = checkpoint['training_dict']['input_size']
+    output_size = checkpoint['training_dict']['output_size']
+    hidden_layers = checkpoint['training_dict']['hidden_layers']
+    dropout_p = checkpoint['training_dict']['dropout_p']
+        
+    model, device = build_model(pre_trained_network=pre_trained_network,
+                                input_size=input_size, output_size=output_size, 
+                                hidden_layers=hidden_layers,
+                                dropout_p=dropout_p)
+
+    model.load_state_dict(checkpoint['state_dict'])
+
+    performance_dict = checkpoint['performance_dict']
+    mapping_dict = checkpoint['mapping_dict']
+    model.class_to_idx = mapping_dict
+    
+    return model, device, performance_dict, mapping_dict
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    size_short_side = 256
+    width = 224
+    height = 224
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    
+    # Resize Image
+    ratio = float(size_short_side/image.size[0])
+    new_size = (size_short_side, int(image.size[1]*ratio))
+    image = image.resize(new_size)
+    
+    # crop out center 224x224 portion
+    left_offset = int((image.size[0]-width)/2)
+    upper_offset = int((image.size[1]-height)/2)
+    box = (left_offset, upper_offset, left_offset+width, upper_offset+height)
+    image = image.crop(box)
+
+    # Convert to numpy array, normalize, and reorder dimensions  
+    np_image = np.array(image)
+    np_image = (np_image/255 - mean)/std
+    np_image = np_image.transpose(2, 0, 1)
+    
+    return np_image
+
+def imshow(image, ax=None, title=None):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+    
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = image.transpose((1, 2, 0))
+    
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+    
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+    
+    ax.imshow(image)
+    
+    return ax
+
+def predict(image_path, model, topk=5):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+        return probabilities and classes
+    '''
+    # Implement the code to predict the class from an image file
+    
+    # Load image
+    image = Image.open(image_path)
+
+    # Convert image into a pytorch tensor
+    np_image = process_image(image)
+    tensor_image = torch.from_numpy(np_image).type(torch.FloatTensor)
+    tensor_image = tensor_image.unsqueeze(dim=0) # torch.Tensor, torch.Size([3, 224, 224])
+
+    # Make a prediction
+    tensor_image = tensor_image.to(device)
+    with torch.no_grad():
+        model.eval()
+        output = model.forward(tensor_image)
+
+    # Calculate probabilites and most likely classes
+    probabilities = torch.exp(output)
+    top_p, top_class = probabilities.topk(5, dim=1)
+
+    # Convert top_p to a list
+    probs = list(top_p.cpu().numpy().squeeze())
+
+    # Figure out the flower names and classes
+    flower_classes, flower_names = [], []
+    for idx in top_class.cpu().numpy().squeeze():
+        flower_class = list(model.class_to_idx.keys())[list(model.class_to_idx.values()).index(idx)]
+        flower_name = cat_to_name[flower_class]
+        flower_classes.append(flower_class)
+        flower_names.append(flower_name)
+
+    classes = flower_classes
+        
+    return probs, classes
+
+def display_and_plot_classes(image_path, classes):
+    original_image = Image.open(image_path)
+    np_image = process_image(original_image)
+
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = np_image.transpose((1, 2, 0))
+    
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+    
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+    
+    # Figure out the flower names
+    flower_names = []
+
+    for flower_class in classes:
+        flower_name = cat_to_name[flower_class]
+        flower_names.append(flower_name)
+
+    # Plot the original image and the probabilites of the top classes
+    fig, (ax1, ax2) = plt.subplots(figsize = (15,4), ncols=2)
+    ax1.imshow(image)
+    ax1.axis('off')
+    ax1.set_title('Actual Flower Name Goes Here')
+    ax2.barh(np.arange(5), top_p.cpu().numpy().squeeze())
+    ax2.set_yticks(np.arange(5))
+    ax2.set_yticklabels(flower_names)
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1.1)
--- Project 2: Image Classifier/train.py
+++ Project 2: Image Classifier/train.py
@@ -1,14 +1,77 @@
 import argparse
+import torch
+from torch import nn, optim
+from torchvision import datasets, transforms
+import cl_helper
+from workspace_utils import active_session, keep_awake
+
 parser = argparse.ArgumentParser()
 parser.add_argument("data_dir", help="data directory of the training files", type=str)
-parser.add_argument("--save_dir", help="directory to save the training checkpoint", type=str)
+parser.add_argument("--save_dir", default='', help="directory to save the training checkpoint", type=str)
 parser.add_argument("--arch", help="model archictecture, ie. 'vgg13'", type=str)
-parser.add_argument("--learning_rate", help="learning rate", type=float)
-parser.add_argument("--hidden_units", help="hidden units in list form", type=list)
-parser.add_argument("--epochs", help="number of epochs to train", type=int)
+parser.add_argument("--learning_rate", default=0.0003, help="learning rate", type=float)
+parser.add_argument("--hidden_units", default=[4096, 512], help="hidden units in list form", type=list)
+parser.add_argument("--epochs", default=1, help="number of epochs to train", type=int)
+parser.add_argument("--max_steps", default=None, 
+                    help="maximum of batches to train, None=Unlimited", type=int)
+parser.add_argument("--print_every", default=20, 
+                    help="number of batches between validations and print updates", type=int)
 parser.add_argument("--gpu", help="enable gpu for training", action="store_true")
 args = parser.parse_args()
-print('Training in Progress.....')
-print('Epoch: __ Training Loss: __ Validation Loss: __ Validation Accuracy: __')
+
+# Define your transforms for the training, validation, and testing sets
+batch_size = 64
+mean_norm = [0.485, 0.456, 0.406]
+std_norm = [0.229, 0.224, 0.225]
+
+train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                      transforms.RandomResizedCrop(224),
+                                      transforms.RandomHorizontalFlip(),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize(mean = mean_norm,
+                                                          std = std_norm)])
+
+test_valid_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(mean = mean_norm,
+                                                            std = std_norm)])
+
+# Load the datasets with ImageFolder
+train_dir = args.data_dir + '/train'
+valid_dir = args.data_dir + '/valid'
+
+train_data = datasets.ImageFolder(train_dir, transform = train_transforms)
+valid_data = datasets.ImageFolder(valid_dir, transform = test_valid_transforms)
+
+# Using the image datasets and the trainforms, define the dataloaders
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle = True)
+validloader = torch.utils.data.DataLoader(valid_data, batch_size=batch_size)
+
+# Construct a model and its training parameters
+
+model, device = cl_helper.build_model(hidden_layers=args.hidden_units, dropout_p=0.5)
+criterion = nn.NLLLoss()
+
+# Only train the classifier parameters, feature parameters are frozen
+optimizer = optim.Adam(model.classifier.parameters(), lr = args.learning_rate)
+
+# Train your network
+
+with active_session():
+    print('Training in Progress.....')
+    performance_dict, training_dict  = cl_helper.train(model, device, trainloader, validloader, 
+                                                    criterion, optimizer, 
+                                                    epochs = args.epochs, 
+                                                    print_every = args.print_every, 
+                                                    max_steps=args.max_steps)
+
 print('Training Complete')
-print('Check point saved in {}'.format(args.save_dir))
+
+# TODO: Save the checkpoint 
+filepath = args.save_dir + 'checkpoint.pth'
+
+model.class_to_idx = train_data.class_to_idx
+mapping_dict = model.class_to_idx
+cl_helper.save_checkpoint(filepath, model, performance_dict, training_dict, mapping_dict)
+print('Check point saved in {}'.format(filepath))
